cd77a6c17e884f5d0f4ee417a9889b2a6ca41844
DevashishDas3 <dedas2024@gmail.com>
2023-07-12 10:04:31 -0400

    change

--- test_hello.py
+++ test_hello.py
@@ -5,34 +5,35 @@ import hello
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello(), "Hello, world!")
+        self.assertNotEqual(hello.hello(), "hey")
 
     def test_add(self):
         self.assertEqual(hello.add(0,1), 1)
-        self.assertEqual(hello.add(2,6), 8)
+        self.assertNotEqual(hello.add(2,6), 7)
     
     def test_sub(self):
         self.assertEqual(hello.sub(0,1), -1)
-        self.assertEqual(hello.sub(2,6), -4)
+        self.assertNotEqual(hello.sub(2,6), -2)
     
     def test_mul(self):
         self.assertEqual(hello.mul(3,5), 15)
-        self.assertEqual(hello.mul(2,6), 12)
+        self.assertNotEqual(hello.mul(2,6), 14)
     
     def test_div(self):
         self.assertEqual(hello.div(0,1), 0)
-        self.assertEqual(hello.div(6,2), 3)
+        self.assertNotEqual(hello.div(6,2), 4.33)
     
     def test_sqrt(self):
         self.assertEqual(hello.sqrt(4), 2)
-        self.assertEqual(hello.sqrt(9), 3)
+        self.assertNotEqual(hello.sqrt(9), 3.1)
     
     def test_power(self):
         self.assertEqual(hello.power(2,3), 8)
-        self.assertEqual(hello.power(2,2), 4)
+        self.assertNotEqual(hello.power(2,2), 7)
     
     def test_log(self):
         self.assertEqual(hello.log(1), 0)
-        # self.assertEqual(hello.log(10), 2.0)
+        self.assertNotEqual(hello.log(10), 3)
     
     def test_exp(self):
         self.assertEqual(hello.exp(0), 1)
